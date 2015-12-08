0638f36052059d7308e043738c3b2b73e506a16d
wuyou8933 <you.wu@fireeye.com>
2015-12-08 13:35:21 -0800

    leet2
    leet2

---
+++ leet2.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        head = ListNode(0)
+        l = head
+        carry = 0
+        while l1 or l2 or carry:
+            sum, carry = carry, 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            if sum > 9:
+                carry = 1
+                sum -= 10
+            l.next = ListNode(sum)
+            l = l.next
+        return head.next
