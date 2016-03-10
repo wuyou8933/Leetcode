6c130d8b9e47ae215054f5d47f5d5e78cb1bed45
You Wu <you.wu@zolabooks.com>
2016-03-09 22:39:28 -0800

    14

---
+++ longest_common_prefix.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs: return ''
+        i = 0
+        while i < len(min(strs, key = len)):
+            if len({s[i] for s in strs}) != 1: break
+            i += 1
+        return strs[0][:i]
